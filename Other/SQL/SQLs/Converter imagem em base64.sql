e7823fb869440c6c5822b229869520b917a0954e
Oséias Gomes <oseias1235@hotmail.com>
2024-05-17 14:53:56 -0300

    meus sqls

--- Other/SQL/SQLs/Converter imagem em base64.sql
+++ Other/SQL/SQLs/Converter imagem em base64.sql
@@ -1,9 +1,7 @@
 SELECT 
-	documento,
-    base64_img1 = CAST('' AS XML).value('xs:base64Binary(sql:column("img1"))','VARCHAR(MAX)')
+    IMAGEM = CONCAT('data:image/webp;base64,', CAST('' AS XML).value('xs:base64Binary(sql:column("IMG"))','VARCHAR(MAX)'))
 FROM (
-    SELECT 
-	CONCAT('perfil_',documento,'.png;#;') as documento,
-        img1 = CAST(foto as varbinary(max))
-    FROM Visitante
-    ) T
+	SELECT
+		IMG = CAST(IMAGEM AS VARBINARY(MAX))
+	FROM POR_VISITANTE WHERE IMAGEM IS NOT NULL
+) TEMPTABLE
